17d08fe278734f6f19ae9e6421f8d71e61d30994
Maryna Bonko <bonko23@gmail.com>
2023-05-23 00:14:44 +0300

    1-4 without css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import user from '../src/components/user.json';
+import data from '../src/components/data.json';
+import friends from '../src/components/friends.json';
+import transactions from '../src/components/transactions.json';
+// import { App } from 'components/App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => (
+  <div className="profile">
+    <div className="description">
+      <img src={avatar} alt={username} className="avatar" />
+      <p className="name">{username}</p>
+      <p className="tag">{tag}</p>
+      <p className="location">{location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{likes}</span>
+      </li>
+    </ul>
+  </div>
 );
+
+const Statistics = ({ title, stats }) => (
+  <section className="statistics">
+    <h2 className="title">{title}</h2>
+
+    <ul className="stat-list">
+      {stats.map(stat => (
+        <li className="item" key={stat.id}>
+          <span className="label">{stat.label}</span>
+          <span className="percentage">{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+const FriendList = ({ friends }) => (
+  <ul className="friend-list">
+    {friends.map(friend => (
+      <FriendListItem key={friend.id} {...friend} />
+    ))}
+  </ul>
+);
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className="item">
+    <span className={isOnline ? 'online' : 'offline'}></span>
+    <img className="avatar" src={avatar} alt={name} width="48" />
+    <p className="name">{name}</p>
+  </li>
+);
+
+const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
